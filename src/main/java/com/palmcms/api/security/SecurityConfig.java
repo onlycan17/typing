2530f7ea14c027610b5a6e3289aa01ddc2979504
ray <ray7.cho@gmail.com>
2020-11-19 13:05:00 +0900

    Manager 정보 조회 수정

--- src/main/java/com/palmcms/api/security/SecurityConfig.java
+++ src/main/java/com/palmcms/api/security/SecurityConfig.java
@@ -26,7 +26,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
