c1aad585a78e785016639067d07f711e6d7f3b67
OhJin-Seok <onlycan17@gmail.com>
2020-12-30 17:01:53 +0900

    # CMS신청관리 매니저 관리자 엑셀 다운로드 기능 추가

--- src/main/java/com/palmcms/api/cms/CmsManagerController.java
+++ src/main/java/com/palmcms/api/cms/CmsManagerController.java
@@ -2,6 +2,7 @@ package com.palmcms.api.cms;
 
 import com.github.pagehelper.Page;
 import com.palmcms.api.common.Constants;
+import com.palmcms.api.common.excel.ExcelGenarator;
 import com.palmcms.api.domain.DTO.CmsApplicationDTO;
 import com.palmcms.api.domain.DTO.UserDTO;
 import com.palmcms.api.domain.VO.DataTableVO;
@@ -14,6 +15,8 @@ import com.palmcms.api.security.AuthoritiesConstants;
 import com.palmcms.api.security.PalmToken;
 import com.palmcms.api.security.SecurityUtils;
 import io.swagger.annotations.ApiOperation;
+import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
@@ -81,14 +84,41 @@ public class CmsManagerController {
     @ApiOperation(value="CMS 신청이력 상세 조회", notes="CMS 신청이력 상세 조회")
     public ResultVO<CmsApplicationDTO> one(@PathVariable Integer appId) {
 
-        UserDTO userDTO = SecurityUtils.getCurrentToken().get().getUserDTO();
+      UserDTO userDTO = SecurityUtils.getCurrentToken().get().getUserDTO();
 
-        Optional<CmsApplicationDTO> oCmsApplicationDTO = cmsService.getAppOneByManagerUserId(userDTO.getId(), appId);
-        if ( oCmsApplicationDTO.isEmpty())
-        {
-            return new ResultVO<>(ResultVO.FAIL, "NOF FOUND");
-        }
+      Optional<CmsApplicationDTO> oCmsApplicationDTO = cmsService
+          .getAppOneByManagerUserId(userDTO.getId(), appId);
+      if (oCmsApplicationDTO.isEmpty()) {
+        return new ResultVO<>(ResultVO.FAIL, "NOF FOUND");
+      }
 
-        return new ResultVO<>(oCmsApplicationDTO.get());
+      return new ResultVO<>(oCmsApplicationDTO.get());
     }
+
+  @PostMapping(value = {"/cms/excelDownload"}, produces = Constants.APPLICATION_JSON_UTF8_VALUE)
+  @ApiOperation(value = "회원목록엑셀다운로드", notes = "회원목록엑셀다운로드")
+  public void userList(@RequestParam(value = "ItemList[]") List<String> itemList
+      , @RequestParam(value = "title") String strTitle
+      , @RequestParam(required = false) String keywordType
+      , @RequestParam(required = false) String keywordText
+      , HttpServletResponse response) throws IOException {
+
+    UserDTO userDTO = SecurityUtils.getCurrentToken().get().getUserDTO();
+    List<CmsApplicationDTO> list = cmsService
+        .getAppListByManagerUserId(userDTO.getId(), keywordType, keywordText);
+    String[][] dataArray = new String[list.size()][7];
+    int i = 0;
+    for (CmsApplicationDTO userData : list) {
+      dataArray[i][0] = userData.getUserChurchName();
+      dataArray[i][1] = userData.getDepartment();
+      dataArray[i][2] = userData.getUserName();
+      dataArray[i][3] = userData.getUserContactNo();
+      dataArray[i][4] = userData.getProductName();
+      dataArray[i][5] = userData.getCmsAppStatusName();
+      dataArray[i][6] = userData.getCreationDate().toString();
+      i++;
+    }
+    new ExcelGenarator().userListToExcel(dataArray, strTitle, itemList, response);
+
+  }
 }
